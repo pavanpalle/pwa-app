4c356929e825773c5d8330889a1005d63f8614b5
Praveengsnp <praveen2send@gmail.com>
2025-04-01 21:32:36 +0530

    initial commits

---
+++ packages/venia-concept/src/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+import store from './store';
+import './index.css';
+import app from '@magento/peregrine/lib/store/actions/app';
+import Adapter from '@magento/venia-ui/lib/components/Adapter';
+import { registerSW } from './registerSW';
+import Newsletter from "@magento/venia-ui/lib/components/Newsletter";
+
+
+// server rendering differs from browser rendering
+const isServer = !globalThis.document;
+
+// TODO: on the server, the http request should provide the origin
+const origin = isServer
+    ? process.env.MAGENTO_BACKEND_URL
+    : globalThis.location.origin;
+
+// on the server, components add styles to this set and we render them in bulk
+const styles = new Set();
+
+const configureLinks = links => [...links.values()];
+
+const tree = (
+    <Adapter
+        configureLinks={configureLinks}
+        origin={origin}
+        store={store}
+        styles={styles}
+    />
+);
+document.addEventListener("DOMContentLoaded", () => {
+    const newsletterSection = document.getElementById("newsletter-section");
+    console.log("newsletter-section", newsletterSection);
+    if (newsletterSection) {
+        ReactDOM.render(<Newsletter />, newsletterSection);
+    }
+});
+
+if (isServer) {
+    // TODO: ensure this actually renders correctly
+    import('react-dom/server').then(({ default: ReactDOMServer }) => {
+        console.log(ReactDOMServer.renderToString(tree));
+    });
+} else {
+    render(tree, document.getElementById('root'));
+    registerSW();
+
+    globalThis.addEventListener('online', () => {
+        store.dispatch(app.setOnline());
+    });
+    globalThis.addEventListener('offline', () => {
+        store.dispatch(app.setOffline());
+    });
+}
+
---
+++ packages/venia-ui/lib/components/TextInput/textInput.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import { node, shape, string } from 'prop-types';
+import { Text as InformedText } from 'informed';
+import useFieldState from '@magento/peregrine/lib/hooks/hook-wrappers/useInformedFieldStateWrapper';
+
+import { useStyle } from '../../classify';
+import { FieldIcons, Message } from '../Field';
+import defaultClasses from './textInput.module.css';
+
+const TextInput = props => {
+    const {
+        after,
+        before,
+        classes: propClasses,
+        regionError,
+        field,
+        message,
+        placeholder,
+        ...rest
+    } = props;
+    const fieldState = useFieldState(field);
+    const classes = useStyle(defaultClasses, propClasses);
+    var inputClass =
+        fieldState.error || regionError ? classes.input_error : classes.input;
+
+    return (
+        <Fragment>
+            <FieldIcons after={after} before={before}>
+                <InformedText {...rest} className={inputClass} field={field}  placeholder={placeholder}/>
+            </FieldIcons>
+            <Message fieldState={fieldState}>{message}</Message>
+        </Fragment>
+    );
+};
+
+export default TextInput;
+
+TextInput.propTypes = {
+    after: node,
+    before: node,
+    classes: shape({
+        input: string
+    }),
+    field: string.isRequired,
+    message: node
+};
