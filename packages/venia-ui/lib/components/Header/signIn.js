787bb1fb41a412dd88a158513bfd5d7cc8dc873b
pavanpalle <pallepavankumarreddy@gmail.com>
2025-05-08 10:12:35 -0700

    committing missing files

--- packages/venia-ui/lib/components/Header/signIn.js
+++ packages/venia-ui/lib/components/Header/signIn.js
@@ -11,14 +11,15 @@ import Field from '../Field';
 import TextInput from '../TextInput';
 import defaultClasses from './signIn.module.css';
 import { GET_CART_DETAILS_QUERY } from './signIn.gql';
-import LinkButton from '../LinkButton';
+
 import Password from '../Password';
 import FormError from '../FormError/formError';
 import GoogleRecaptcha from '../GoogleReCaptcha';
 
 import userIcon from './user-icon.svg';
 import passwordIcon from './password-icon.svg';
-
+import Checkbox from '../Checkbox';
+import { Link } from 'react-router-dom';
 
 const SignIn = props => {
     const classes = useStyle(defaultClasses, props.classes);
@@ -39,8 +40,6 @@ const SignIn = props => {
 
     const {
         errors,
-        handleCreateAccount,
-        handleEnterKeyPress,
         signinHandleEnterKeyPress,
         handleForgotPassword,
         forgotPasswordHandleEnterKeyPress,
@@ -54,95 +53,103 @@ const SignIn = props => {
         root: classes.forgotPasswordButton
     };
 
-    console.log("recaptchaWidgetProps",recaptchaWidgetProps)
+    console.log('recaptchaWidgetProps', recaptchaWidgetProps);
     return (
-        <div className='flex flex-row'>
+        <div className="flex flex-row">
             <div data-cy="SignIn-root" className={classes.root}>
-                <div className='signin-form-container'>
-                
-                <FormError errors={Array.from(errors.values())} />
-                <Form
-                    getApi={setFormApi}
-                    className={classes.form}
-                    onSubmit={handleSubmit}
-                    data-cy="SignIn-form"
-                    initialValues={initialValues && initialValues}
-                >
-                    <div className='field'>
-                    <Field
-                        id="emailSignIn"
-                        
+                <div className="signin-form-container">
+                    <FormError errors={Array.from(errors.values())} />
+                    <Form
+                        getApi={setFormApi}
+                        className={classes.form}
+                        onSubmit={handleSubmit}
+                        data-cy="SignIn-form"
+                        initialValues={initialValues && initialValues}
                     >
-                        <img src={userIcon} width={24} height={24} title="Username"/>
-                        <TextInput
-                            id="emailSignIn"
-                            data-cy="SignIn-email"
-                            autoComplete="email"
-                            field="email"
-                            validate={isRequired}
-                            data-cy="email"
-                            aria-label={formatMessage({
-                                id: 'global.emailRequired',
-                                defaultMessage: 'Email Required'
+                        <div className="field">
+                            <Field id="emailSignIn">
+                                <img
+                                    src={userIcon}
+                                    width={24}
+                                    height={24}
+                                    title="Username"
+                                />
+                                <TextInput
+                                    id="emailSignIn"
+                                    data-cy="SignIn-email"
+                                    autoComplete="email"
+                                    field="email"
+                                    validate={isRequired}
+                                    data-cy="email"
+                                    aria-label={formatMessage({
+                                        id: 'global.emailRequired',
+                                        defaultMessage: 'Email Required'
+                                    })}
+                                    placeholder="Username"
+                                />
+                            </Field>
+                        </div>
+                        <div class="field">
+                            <img
+                                src={passwordIcon}
+                                width={24}
+                                height={24}
+                                title="Passowrd"
+                            />
+                            <Password
+                                data-cy="SignIn-password"
+                                fieldName="password"
+                                id="Password"
+                                validate={isRequired}
+                                autoComplete="current-password"
+                                isToggleButtonHidden={false}
+                                data-cy="password"
+                                aria-label={formatMessage({
+                                    id: 'global.passwordRequired',
+                                    defaultMessage: 'Password Required'
+                                })}
+                                placeholder="Password"
+                            />
+                        </div>
+                        <Checkbox
+                            field="stayLoggedIn"
+                            id="stayLoggedIn"
+                            label={formatMessage({
+                                id: 'SignIn.stayLoggedIn',
+                                defaultMessage: 'Stay Logged In'
                             })}
-                            placeholder="Username"
                         />
-                    </Field>
-                    </div>
-                    <div class="field">
-                        <img src={passwordIcon} width={24} height={24} title='Passowrd' />
-                        <Password
-                        data-cy="SignIn-password"
-                        fieldName="password"
-                        id="Password"
-                        
-                        validate={isRequired}
-                        autoComplete="current-password"
-                        isToggleButtonHidden={false}
-                        data-cy="password"
-                        aria-label={formatMessage({
-                            id: 'global.passwordRequired',
-                            defaultMessage: 'Password Required'
-                        })}
-                        placeholder="Password"                        
-                    />
-
-                    </div>
-                    <div className={classes.forgotPasswordButtonContainer}>
-                        <LinkButton
-                            classes={forgotPasswordClasses}
-                            type="button"
-                            onClick={handleForgotPassword}
-                            onKeyDown={forgotPasswordHandleEnterKeyPress}
-                            data-cy="SignIn-forgotPasswordButton"
-                        >
-                            <FormattedMessage
-                                id={'signIn.forgotPasswordText'}
-                                defaultMessage={'Forgot Password?'}
-                            />
-                        </LinkButton>
-                    </div>
-                    <GoogleRecaptcha {...recaptchaWidgetProps} />
-                    <div className={classes.buttonsContainer}>
-                        <Button
-                            priority="high"
-                            type="submit"
-                            onKeyDown={signinHandleEnterKeyPress}
-                            data-cy="SignInButton-root_highPriority"
-                            disabled={Boolean(isBusy)}
-                        >
-                            <FormattedMessage
-                                id={'signIn.signInText'}
-                                defaultMessage={'Login'}
-                            />
-                        </Button>
-                    
-                    </div>
-                    
-                </Form>
+                        <div className={classes.forgotPasswordButtonContainer}>
+                            <Link
+                                classes={forgotPasswordClasses}
+                                to={'/forgot-password'}
+                            >
+                              
+                                <FormattedMessage
+                                    id={'signIn.forgotPasswordText'}
+                                    defaultMessage={'Forgot Password?'}
+                                />
+                            </Link>
+                            
+                        </div>
+                        <GoogleRecaptcha {...recaptchaWidgetProps} />
+                        <div className={classes.buttonsContainer}>
+                            <Button
+                                priority="high"
+                                type="submit"
+                                onKeyDown={signinHandleEnterKeyPress}
+                                data-cy="SignInButton-root_highPriority"
+                                disabled={Boolean(isBusy)}
+                            >
+                                <FormattedMessage
+                                    id={'signIn.loginInText'}
+                                    defaultMessage={'Login'}
+                                />
+                            </Button>
+                        </div>
+                    </Form>
                 </div>
             </div>
-           
         </div>
     );
 };
